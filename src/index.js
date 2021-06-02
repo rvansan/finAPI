e1960c0f7466efb6d34f6ec4dcacb0a65c35f9f4
Rafael Vansan <rafaelvansan@MacBook-Air-de-Rafael.local>
2021-06-02 20:56:31 -0300

    02- Cadastro de conta

--- src/index.js
+++ src/index.js
@@ -1,9 +1,25 @@
 const express = require('express');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
+app.use(express.json());
 
-app.get('/', (req, res) => {
-    return res.json({ok: 'true!'})
+const customers = [];
+
+app.get('/', (request, response) => {
+    return response.json({ok: 'true!'})
+});
+
+app.post('/account', (request, response) => {
+    const { cpf, name } = request.body;
+    const id = uuidv4();
+    customers.push({
+        id,
+        cpf,
+        name,
+        statement: []
+    });
+    return response.status(201).send();
 });
 
 app.listen('3333');
