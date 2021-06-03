66e0b8a6fe717ffda49898f4222423f7872a2f1f
Rafael Vansan <rafaelvansan@MacBook-Air-de-Rafael.local>
2021-06-03 15:09:34 -0300

    08- Criando saque na conta

--- src/index.js
+++ src/index.js
@@ -18,6 +18,21 @@ function verifyIfExistsAccountCPF(request, response, next) {
     return next();
 }
 
+function getBalance(statement) {
+    const balance = statement.reduce((acc, operation) => {
+       if(operation.type === 'credit'){
+           return acc + operation.amount;
+       } 
+       else {
+           return acc - operation.amount;
+       }
+    }, 0);
+
+    return balance;
+}
+
+/************************************************* */
+
 
 app.get('/', (request, response) => {
     return response.json({ok: 'true!'})
@@ -58,10 +73,33 @@ app.post('/deposit', verifyIfExistsAccountCPF, (request, response) => {
         amount,
         description,    
         created_at: new Date(),
-        type: 'credito'
+        type: 'credit'
     }
     customer.statement.push(statementOperation);
     return response.status(201).send();
+});
+
+app.post('/withdraw', verifyIfExistsAccountCPF,(request, response) => {
+    const { amount } = request.body;
+    const { customer } = request;
+    const balance = getBalance(customer.statement);
+console.log(balance);
+    if(amount > balance) {
+        return response
+            .status(400)
+            .json({error: 'Insufficient funds!'});
+    }
+
+    const statementOperation = {
+        amount,   
+        created_at: new Date(),
+        type: 'debit'
+    }
+
+    customer.statement.push(statementOperation);
+    return response.status(201).send();
+
+
 });
 
 app.listen('3333');
